384ee59a53f818c954d8a3b55e9e7c51e676cbbf
esakkipraneetha-del <esakkipraneetha@gmail.com>
2026-06-15 18:49:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
